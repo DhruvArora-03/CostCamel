959dde00972b95d929d20ec733bc2816d43bf6d8
DhruvArora-03 <dhruvarora@utexas.edu>
2023-06-23 21:40:34 -0500

    Created `Container` - styled.View

---
+++ CoinCamel/components/shared.tsx
@@ -0,0 +1,9 @@
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+import colors from "./colors";
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: ${colors.white};
+`;
