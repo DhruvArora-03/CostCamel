9f97d0db498f69796f04563743dce9f168cc82e4
DhruvArora-03 <dhruvarora@utexas.edu>
2023-06-23 21:39:51 -0500

    colors

---
+++ CoinCamel/components/colors.tsx
@@ -0,0 +1,12 @@
+const colors = {
+  white: "#fff",
+  primary: "#ef835d",
+  secondary: "#2c365a",
+  tertiary: "#85c6d8",
+  gray: "#d1d5db",
+  graylight: "#F3F4F6",
+  graydark: "#485563",
+  accent: "#fbcd77",
+};
+
+export default colors;
